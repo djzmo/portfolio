f20671de13bbb2b3f81a28d327571fcae7964dc8
Yana <me@djzmo.com>
2020-11-02 20:58:44 +0700

    remove send message contact form

--- components/sections/contact/contact.tsx
+++ components/sections/contact/contact.tsx
@@ -10,23 +10,13 @@ interface IProps {
 const Contact: React.StatelessComponent<IProps> = ({socialIconList}) => {
   return (
     <section className="section section-theme">
-      <h2>Contact</h2>
+      <h2>Get in touch</h2>
 
       <div className="contact-container">
         <div className="social-container">
           {socialIconList.map((social, i) => (
             <SocialIcon key={i} social={social} />
           ))}
-        </div>
-
-        <div className="form-container">
-          <h2>Send me a message</h2>
-          <form method="POST" className="form" action="https://formspree.io/harvey@harveydelaney.com">
-            <input type="hidden" name="_subject" value="Contact request from personal website"/>
-            <input type="email" name="_replyto" placeholder="Your email" /> 
-            <textarea name="message" placeholder="Your message" />
-            <button type="submit">Send</button>
-          </form>
         </div>
       </div>
     </section>
