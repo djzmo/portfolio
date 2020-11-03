34f0da20862e6a26034f0d1b8567c1bf63cb31bb
Yana <me@djzmo.com>
2020-11-03 22:46:46 +0700

    add next-css

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 const withTypescript = require('@zeit/next-typescript')
 const withSass = require('@zeit/next-sass')
+const withCss = require('@zeit/next-css')
 
-module.exports = withTypescript(withSass({
+module.exports = withTypescript(withSass(withCss({
   assetPrefix: process.env.BASE_PATH || '',
   publicRuntimeConfig: {
     basePath: process.env.BASE_PATH || '',
   }
-}));
+})));
