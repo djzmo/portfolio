cc9e718fe6b574a5f7e2a5e3405be5a5cd47a820
Yana <me@djzmo.com>
2020-11-03 22:47:40 +0700

    revamp skills layout

--- components/sections/skills/skills.tsx
+++ components/sections/skills/skills.tsx
@@ -2,25 +2,47 @@ import * as React from "react";
 
 import "./skills.scss";
 
-import Skill from "../../shared/skill/skill";
-import { SkillEnum } from "../../../constants";
-
 interface IProps {
-  skillList: SkillEnum[];
+  categoryList: ISkillCategory[];
 }
 
-const Skills: React.StatelessComponent<IProps> = ({skillList}) => {
-  return (
-    <section className="section section-primary">
-      <h2>Skills</h2>
+export interface ISkillCategory {
+  category: string;
+  workingKnowledge: string[];
+  basicKnowledge: string[];
+}
 
-      <div className="skills-container">
-        {skillList.map((skill, i) => (
-          <Skill key={i} skill={skill} size="large" />
-        ))}
-      </div>
-    </section>
-  );
-};
+class Skills extends React.Component<IProps, {}> {
+  public render() {
+    const {categoryList} = this.props;
+    return (
+        <section className="section section-primary">
+          <h2>Skills at a Glance</h2>
+          <div className="skills-container">
+            {categoryList.map((category) => {
+              return (
+                  <div className="category-column">
+                    <h4>{category.category}</h4>
+                    <p><b>Working Knowledge</b></p>
+                    <ul>
+                      {category.workingKnowledge.map((skill) => {
+                        return (<li>{skill}</li>);
+                      })}
+                    </ul>
+                    <p><b>Basic Knowledge</b></p>
+                    <ul>
+                      {category.basicKnowledge.map((skill) => {
+                        return (<li>{skill}</li>);
+                      })}
+                    </ul>
+                  </div>
+              )
+            })}
+          </div>
+        </section>
+    );
+  }
+}
+
 
 export default Skills;
