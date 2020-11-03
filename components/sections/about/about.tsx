fa25e0034faa56daec2d21ec477d4c311da3a66e
Yana <me@djzmo.com>
2020-11-03 22:48:30 +0700

    revamp about section

--- components/sections/about/about.tsx
+++ components/sections/about/about.tsx
@@ -1,29 +1,62 @@
 import * as React from "react";
 
 import "./about.scss";
+import {IUserDetails} from "../landing/landing";
+import {SkillEnum} from "../../../constants";
+import Skill from "../../shared/skill/skill";
 
-const renderAboutText = (aboutTextList: string[]) => (
-  <div className="about-text">
-    {aboutTextList.map((textSection, i) => (
-      <p key={i}>{textSection}</p>
-    ))}
-  </div>
+const renderSkillSummary = (skillSummaryList: SkillEnum[]) => {
+  if (skillSummaryList != null) {
+    return (
+        <>
+          <p>Here are a few technologies I've been working with recently:</p>
+          <div className="skills-summary-container">
+            {skillSummaryList.map((skill, i) => (
+                <Skill key={i} skill={skill} size="small"/>
+            ))}
+          </div>
+        </>
+    )
+  }
+};
+
+const renderAboutText = (aboutTextList: string[], skillSummaryList: SkillEnum[]) => (
+    <div className="about-text">
+      {aboutTextList.map((textSection, i) => (
+          <p key={i} dangerouslySetInnerHTML={{__html: textSection}}/>
+      ))}
+
+      {renderSkillSummary(skillSummaryList)}
+    </div>
 );
 
 interface IProps {
+  userDetails: IUserDetails;
   aboutTextList: string[];
+  aboutSummaryTextList: string[];
+  skillSummaryList?: SkillEnum[];
 }
 
-const About: React.StatelessComponent<IProps> = ({aboutTextList}) => {
-  return (
-    <section className="section section-primary section-about">
-      <h2>About Harvey</h2>
-      <div className="content-container about-container">
-        <img className="portrait" src='/static/images/profile-picture.jpg' />
-        {renderAboutText(aboutTextList)}
-      </div>
-    </section>
-  );
-};
+class About extends React.Component<IProps, {}> {
+  public render() {
+    const {userDetails, aboutTextList, aboutSummaryTextList, skillSummaryList} = this.props;
+
+    return (
+        <section className="section section-primary section-about">
+          <h2>About {userDetails.nickname}</h2>
+          <div className="content-container about-container">
+            <img className="portrait" src='/static/images/profile-picture.png'/>
+
+            <div className="about-full-container">
+              {renderAboutText(aboutTextList, skillSummaryList)}
+            </div>
+            <div className="about-summary-container">
+              {renderAboutText(aboutSummaryTextList, skillSummaryList)}
+            </div>
+          </div>
+        </section>
+    );
+  }
+}
 
 export default About;
